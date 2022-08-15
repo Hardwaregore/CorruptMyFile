a8b33917473e28dba452da046151d28367f209fe
Hardwaregore <hardwaregore101@gmail.com>
2022-08-15 14:58:44 -0500

    Update corrupt.sh

--- corrupt.sh
+++ corrupt.sh
@@ -2,13 +2,9 @@
 
 read -p "What to corrupt? " file
 
-echo "Getting more info about the file..."
-
-bytes = $(expr $(cat $file | wc -c) \* 8)
-
 echo "Corrupting file..."
 
-sudo dd if=/dev/zero of=$file bs=1 count=$bytes
+sudo dd if=/dev/zero of=$file bs=1 count=1024
 
 echo "Your file is ready"
 
