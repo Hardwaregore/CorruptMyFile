28663a6dea2a7d028cf9399c6fb664b0449c0095
Hardwaregore <hardwaregore101@gmail.com>
2022-08-15 14:49:41 -0500

    Update corrupt.sh

--- corrupt.sh
+++ corrupt.sh
@@ -2,6 +2,14 @@
 
 read -p "What to corrupt? " file
 
+echo "Getting more info about the file..."
+
 bytes = $(expr $(cat exploit.py | wc -c) \* 8)
 
+echo "Corrupting file..."
+
 sudo dd if=/dev/zero of=$file bs=1 count=$bytes
+
+echo "Your file is ready"
+
+exit
