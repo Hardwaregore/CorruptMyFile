5456f0c8c7164952182a6cf5800ec4258cb1c9c4
Hardwaregore <hardwaregore101@gmail.com>
2022-08-15 15:06:14 -0500

    Update corrupt.sh

--- corrupt.sh
+++ corrupt.sh
@@ -4,7 +4,7 @@ read -p "What to corrupt? " file
 
 echo "Corrupting file..."
 
-sudo dd if=/dev/zero of=$file bs=1 count=1024
+sudo dd if=/dev/random of=$file bs=1 count=10240
 
 echo "Your file is ready"
 
